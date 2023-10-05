589d61d3b0882dab426e219fd943c0a75573043c
SidduLakkarse <SidduLakkarse>
2023-10-05 13:22:44 +0530

    Input Decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularFullCourse';
+  parentMessage = 'Message coming from parent Component.'
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+
+  @Input() fromParent: string | undefined;
+
+  constructor() { };
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
